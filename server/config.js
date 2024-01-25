eb1f89d645463ce367f736b3b3562dcd5249fd07
Edinson <edisonprise@hotmail.com>
2024-01-25 12:51:46 -0500

    modelos de datos

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://edisonprise:ifnNR8ViTaMWNKqq@crud.b5ymota.mongodb.net/postsdb";
+export const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://edisonprise:ifnNR8ViTaMWNKqq@crud.b5ymota.mongodb.net/admin";
 export const PORT = process.env.PORT || 3000;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-import express from "express";
-import postsRoutes from "./routes/posts.routes.js";
+import  app  from './app.js'
 import { connectDB } from "./db.js";
 import { PORT } from "./config.js";
 
-const app = express();
-connectDB();
 
-app.use(postsRoutes);
+connectDB();
 
 app.listen(PORT);
 console.log("Server is running on port", PORT);
