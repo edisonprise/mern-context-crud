c9b11784bc3e35e2719de4cd3218464951b70213
Edinson <edisonprise@hotmail.com>
2024-01-25 12:22:00 -0500

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import postsRoutes from "./routes/posts.routes.js";
+import { connectDB } from "./db.js";
+import { PORT } from "./config.js";
 
 const app = express();
+connectDB();
 
 app.use(postsRoutes);
 
-app.listen(4000);
-console.log("Server is running on port", 4000);
+app.listen(PORT);
+console.log("Server is running on port", PORT);
